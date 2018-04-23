d196614fc94fa66bfb44d24dfbafa9e7fdf4c95d
yahav erlich <yahaverlich@gmail.com>
2018-04-22 22:11:01 -0700

    Fixed HTML bugs and made backend skeleton for data handling

--- server.js
+++ server.js
@@ -1,43 +1,39 @@
 const express = require('express');
-const http = require('http');
 const handlebars = require('handlebars')
+const bodyParser = require("body-parser");
 const path = require('path');
 const app = express();
-const bodyParser = require('body-parser');
 
-const account = require ('./routes/account');
-const kitchen = require('./routes/kitchen');
-const recipeList = require ('./routes/recipeList');
-const recipeResult = require('./routes/recipeResult');
-
-// all environments
-/*app.use(bodyParser.urlencoded({extended: true}));
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('IxD secret key'));
-app.use(express.session());
-app.use(app.router); */
 app.use(express.static(path.join(__dirname, 'public')));
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Check the login credentials
+app.post('/login.html', (req, res) => {
+  console.log("Username: " + req.body.user);
+  console.log("Password: " + req.body.pass);
 
+  // Check to see if the username and password are in the database
 
-app.get('/account', account.view);
-app.get('/kitchen', kitchen.view);
-app.get('/recipeList', recipeList.view);
-app.get('/recipeResult', recipeResult.view);
+});
 
+// Grab ingredients list and manipulate it
+app.post('/kitchen.html', (req, res) => {
 
+  console.log("Fridge List: ");
+  for(const i of req.body.fridge){
+    console.log(i);
+  }
+  
+  console.log("Spice Rack: " + req.body.spices);
+  console.log("Cupboard: " + req.body.cupboard);
 
+  // Check to see if the username and password are in the database
 
+});
 
 
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000/");
-})
+});
