5e726be085b7c361239d8298c971be8bedce98b7
yahav erlich <yahaverlich@gmail.com>
2018-04-23 12:56:13 -0700

    recipeList can now receive data from server, all the ingredients stored in the DB

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Database
+const my_ingredients = {};
+
 // Check the login credentials
 app.post('/login.html', (req, res) => {
   console.log("Username: " + req.body.user);
@@ -21,24 +24,37 @@ app.post('/login.html', (req, res) => {
 // Grab ingredients list and manipulate it
 app.post('/kitchen.html', (req, res) => {
 
+  // Object of all ingredient types
   const my_list = req.body;
 
+  // List of each type of ingredient
   const fridge_list = my_list['fridge[]'];
   const spice_rack = my_list['spices[]'];
   const cupboard = my_list['cupboard[]'];
+
+  // Add all the ingredients to the database
+  my_ingredients.fridge = fridge_list;
+  my_ingredients.spices = spice_rack;
+  my_ingredients.cupboard = cupboard;
 
   //TODO: check whether these 3 lists are empty or not
   if(fridge_list == undefined){};
   if(spice_rack == undefined){};
   if(cupboard == undefined){};
 
-console.log(my_list);
+  // Print ingredients list
   console.log("Fridge List: " + fridge_list);
   console.log("Spice Rack: " + spice_rack);
   console.log("Cupboard: " + cupboard);
 
 });
 
+// Grab all of the user recipes from the DB and send them to the users
+//TODO: once routes are implemented, can make this a Get request that triggers when page loads,
+// rather than having to make an ajax post request. then input the route name
+app.post('/recipeList.html', (req, res) => {
+  res.send(my_ingredients);
+});
 
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000/");
