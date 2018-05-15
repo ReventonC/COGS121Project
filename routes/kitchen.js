2b81366d3d977f5f1f19f408c3af6e85eaa2bc93
Aaron Wong <flamingc4@gmail.com>
2018-05-14 22:28:07 -0700

    idk how to render the data

--- routes/kitchen.js
+++ routes/kitchen.js
@@ -1,6 +1,25 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('recipes.db');
 exports.view = function(req,res){
+	//console.log(req);
+	
+	const username = req.cookies.username;	
+	console.log(res);
+
+	db.all(
+		'SELECT * FROM ingredients WHERE username=$user' ,
+		{
+			$user: username,
+		},
+		(err, rows) => {
+			if(err)
+				console.log("Couldn't get ingredients on the kitchen page");
+			else{
+				//Aaron: idk why it prints double ingredients
+				console.log(rows);
+			}
+		}
+
+	);
 	res.render('kitchen');
-	//db.run();
 }
