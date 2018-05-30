1d2a3e0c8ee4908410c193eb2bf8349e09dfd5b5
Aaron Wong <flamingc4@gmail.com>
2018-05-30 00:18:51 -0700

    rendering doesnt work

--- public/js/recipeList.js
+++ public/js/recipeList.js
@@ -1,28 +1,27 @@
 $(document).ready(() => {
     const username = Cookies.get('user'); 
-       $.ajax({
-          type: 'POST',
-          data: {user: username},
-          dataType: 'json',
-          success: (recipes) => {
-              //set up a different click handler for each recipe
-              for(i in recipes){
-                  $(".content").on('click', '.recipeBox', function (){ 
-                       document.cookie = "id=" + recipes[i].id;
-                    console.log(document.cookie);
-                     window.location = "recipeResult";
+      //  $.ajax({
+      //     type: 'POST',
+      //     data: {user: username},
+      //     dataType: 'json',
+      //     success: (recipes) => {
+      //         //set up a different click handler for each recipe
+      //           $(".content").on('click', '.recipeBox', function (){ 
+      //            document.cookie = "id=" + recipes[1].id;
+      //         //console.log(document.cookie);
+      //          //window.location = "recipeResult";
                        
-              });
-              //go to the recipeResult page through here
+      //         });
+      //         //go to the recipeResult page through here
              
-          };
-
-          //console.log(recipes[0].id);                  
-          // document.cookie = "id=" + recipes.id;
-          // console.log(document.cookie);
           
-          }
-      });
+
+      //     //console.log(recipes[0].id);                  
+      //     // document.cookie = "id=" + recipes.id;
+      //     // console.log(document.cookie);
+          
+      //     }
+      // });
     $("#accountBoxUserName").html(Cookies.get("user"));
 
     const resultListComponent = new Vue({
@@ -33,7 +32,10 @@ $(document).ready(() => {
         methods: {
             isListEmpty: function (e) {
                 return true? this.recipeList.length == 0 : false;
-            }
+            },
+            getId: function(id) {
+              console.log(id);
+            },
         },
         mounted: function() {
             // Grab the current user's recipes
@@ -47,7 +49,7 @@ $(document).ready(() => {
                     for(const r of recipes){
                       this.recipeList.push(r);
                     }
-                }
+                }                
             });
 
         }
