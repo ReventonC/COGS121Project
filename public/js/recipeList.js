915b43a3101a9a9dde6b1454519e1fe3bc3773c4
yahaverlich <yahaverlich@gmail.com>
2018-06-04 14:20:30 -0700

    Update recipeList.js

--- public/js/recipeList.js
+++ public/js/recipeList.js
@@ -10,28 +10,7 @@ $(document).ready(() => {
        //go to the recipeResult page through here
        window.location = "recipeResult";
     });
-      //  $.ajax({
-      //     type: 'POST',
-      //     data: {user: username},
-      //     dataType: 'json',
-      //     success: (recipes) => {
-      //         //set up a different click handler for each recipe
-      //           $(".content").on('click', '.recipeBox', function (){
-      //            document.cookie = "id=" + recipes[1].id;
-      //         //console.log(document.cookie);
-      //          //window.location = "recipeResult";
-
-      //         });
-      //         //go to the recipeResult page through here
-
-
-
-      //     //console.log(recipes[0].id);
-      //     // document.cookie = "id=" + recipes.id;
-      //     // console.log(document.cookie);
-
-      //     }
-      // });
+ 
     $("#accountBoxUserName").html(Cookies.get("user"));
 
     const resultListComponent = new Vue({
@@ -49,7 +28,8 @@ $(document).ready(() => {
             },
         },
         mounted: function() {
-            // Grab the current user's recipes
+            // Make AJAX call to Grab the current user's ingredients and make API call and then display 
+            // the list of recipes
             const username = Cookies.get('user');
             $.ajax({
                 type: 'POST',
